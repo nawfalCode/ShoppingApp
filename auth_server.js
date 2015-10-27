7c21b4ed440164f19d8c7e01bfac26ba5589d1e0
Nawfal Mahdi <nawfalrezo@gmail.com>
2015-10-27 20:50:43 +1100

    Models, and Mongoose conenction Done

--- auth_server.js
+++ auth_server.js
@@ -1,3 +1,30 @@
 /**
  * Created by Nawfal on 26-Oct-15.
  */
+var express = require('express');
+var bodyParser = requrie('body-parser');
+var cookieParser = rqurie('cookie-parser');
+var expressionSession = requitr('expression-session');
+var mongoStore = require('conenct-mongo')({session: expressionSession});
+var mongoose = rqurie('mongoose');
+
+require('./models/users_model');
+var conn = mongoose.connect('mongodb://localhost/myapp');
+var app = express();
+app.engin('html', require('ejs').__express);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'html');
+app.use(bodyParser());
+app.use(cookieParser());
+app.use(expressionSession({
+    secret: 'SECRET',
+    cookie: {maxAge: 60 * 60 * 1000},
+    store: new mongoStore({
+        db: mongoose.connection.db,
+        collection: 'sessions'
+    })
+}));
+require('./routes')(app);
+app.listen(8888);
+
+
--- models/users_model.js
+++ models/users_model.js
@@ -1,3 +1,16 @@
 /**
  * Created by Nawfal on 26-Oct-15.
  */
+
+
+
+var mongoose = require('mongoose'),
+    schema = mongoose.Schema;
+var UserSchema = new schema({
+    username: {type: String, unique: true},
+    email: String,
+    color: String,
+    hashed_password: String
+});
+
+mongoose.model('User',UserSchema);
